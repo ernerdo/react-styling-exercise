6d33ac961b7457099631fb1ecc77f08260d95a75
Jose Silva <jsilva@nicarao.agency>
2023-06-02 13:20:58 -0600

    add data hardcode

--- src/components/ProfileEducation/ProfileEducation.jsx
+++ src/components/ProfileEducation/ProfileEducation.jsx
@@ -1,8 +1,20 @@
-export const ProfileEducation = () => {
+import PropTypes from "prop-types";
+export const ProfileEducation = ({ education, isLastItem }) => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row items-center gap-2 relative">
+      <div className="w-2 h-2 bg-gray-300 rounded-full"></div>
+      {!isLastItem && (
+        <div className="absolute top-[20px] mt-0 left-[0.15rem] w-px h-[0.5rem] border border-r bg-gray-200"></div>
+      )}
       <p>Logo</p>
-      <p>Education title</p>
+      <div className="w-52 overflow-hidden">
+        <p className="truncate">{education}</p>
+      </div>
     </div>
   );
+};
+
+ProfileEducation.propTypes = {
+  education: PropTypes.string,
+  isLastItem: PropTypes.bool,
 };
--- src/components/ProfileExperience/ProfileExperience.jsx
+++ src/components/ProfileExperience/ProfileExperience.jsx
@@ -1,8 +1,20 @@
-export const ProfileExperience = () => {
+import PropTypes from "prop-types";
+export const ProfileExperience = ({ experience, isLastItem }) => {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row items-center gap-2 relative">
+      <div className="w-2 h-2 bg-gray-200 rounded-full"></div>
+      {!isLastItem && (
+        <div className="absolute top-[20px] mt-0 left-[0.15rem] w-px h-[0.5rem] border border-r bg-gray-200"></div>
+      )}
       <p>Logo</p>
-      <p>Education title</p>
+      <div className="w-52 overflow-hidden">
+        <p className="truncate">{experience}</p>
+      </div>
     </div>
   );
+};
+
+ProfileExperience.propTypes = {
+  experience: PropTypes.string,
+  isLastItem: PropTypes.bool,
 };
