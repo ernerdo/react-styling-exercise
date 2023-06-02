131b92a642416f080e6367bccfe298ed38f5572d
Jose Silva <jsilva@nicarao.agency>
2023-06-02 14:50:29 -0600

    add validation for optional data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,26 +10,38 @@ export const Card = ({ person }) => {
     <div className="border border-3 border-slate-300 rounded-lg shadow-xl p-5">
       <div className="flex flex-row p-2 gap-3 items-center h-10">
         <p className="italic font-semibold text-lg">{person.name}</p>
-        <a target="_blank" rel="noreferrer" href={`tel:${person.phone_number}`}>
-          <span className="text-gray-500">
-            <BsTelephone size={18} />
-          </span>
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href={`mailto:${person.personal_email}`}
-        >
-          <span className="text-gray-500">
-            <HiOutlineMail size={18} />
-          </span>
-        </a>
-        <div className="h-full w-px border border-r bg-gray-200"></div>
-        <a target="_blank" rel="noreferrer" href={person.linkedIn}>
-          <span className="text-blue-600">
-            <BsLinkedin size={18} />
-          </span>
-        </a>
+        {person.phone_number && (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`tel:${person.phone_number}`}
+          >
+            <span className="text-gray-500">
+              <BsTelephone size={18} />
+            </span>
+          </a>
+        )}
+        {person.personal_email && (
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`mailto:${person.personal_email}`}
+          >
+            <span className="text-gray-500">
+              <HiOutlineMail size={18} />
+            </span>
+          </a>
+        )}
+        {person.linkedIn && (
+          <>
+            <div className="h-full w-px border border-r bg-gray-200"></div>
+            <a target="_blank" rel="noreferrer" href={person.linkedIn}>
+              <span className="text-blue-600">
+                <BsLinkedin size={18} />
+              </span>
+            </a>
+          </>
+        )}
       </div>
       <div className="flex flex-row p-2 gap-2 items-center">
         <p>{person.current_title}</p>
