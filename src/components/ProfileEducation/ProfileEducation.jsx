847a08e4319e8a7781a7300d2efdae8fc3cd93b4
Jose Silva <jsilva@nicarao.agency>
2023-06-02 15:05:00 -0600

    add responsive card

--- src/components/ProfileEducation/ProfileEducation.jsx
+++ src/components/ProfileEducation/ProfileEducation.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 export const ProfileEducation = ({ education, isLastItem }) => {
   return (
     <div className="flex flex-row items-center gap-2 relative">
-      <div className="w-2 h-2 bg-gray-300 rounded-full"></div>
+      <div className="w-2 h-2 bg-gray-200 rounded-full"></div>
       {!isLastItem && (
         <div className="absolute top-[20px] mt-0 left-[0.15rem] w-px h-[0.5rem] border border-r bg-gray-200"></div>
       )}
